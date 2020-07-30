91e0f1da05ba54b82fae9cf6dd163581df8aa537
danaibavishi <danaibavishi@gmail.com>
2020-07-30 17:06:34 -0500

    Coordinates Normalized + New ML
    Next Steps - Z coordinate fix scaling

--- Unity_Project/Assets/Scripts/LEAP_coordinates.cs
+++ Unity_Project/Assets/Scripts/LEAP_coordinates.cs
@@ -27,15 +27,15 @@ public class LEAP_coordinates : MonoBehaviour
     void Update()
     {
         Hand leap_hand = hand_model.GetLeapHand();
-        if (leap_hand == null) Debug.LogError("No leap_hand found");
-        var hand = Hands.Right;
-        if (hand != null)
+        //if (leap_hand == null) Debug.LogError("No leap_hand found");
+        //var hand = Hands.Right;
+        if (leap_hand != null)
         {
             Leap.Vector indexPosition = leap_hand.Fingers[1].TipPosition * 100;
-            Leap.Vector middlePosition = hand.Fingers[2].TipPosition * 100;
-            Leap.Vector ringPosition = hand.Fingers[3].TipPosition * 100;
-            Leap.Vector pinkyPosition = hand.Fingers[4].TipPosition * 100;
-            Leap.Vector thumbPosition = hand.Fingers[0].TipPosition * 100;
+            Leap.Vector middlePosition = leap_hand.Fingers[2].TipPosition * 100;
+            Leap.Vector ringPosition = leap_hand.Fingers[3].TipPosition * 100;
+            Leap.Vector pinkyPosition = leap_hand.Fingers[4].TipPosition * 100;
+            Leap.Vector thumbPosition = leap_hand.Fingers[0].TipPosition * 100;
             Leap.Vector handCenter = leap_hand.PalmPosition * 100;
 
             float[] index = { 0, 0, 0 };
@@ -43,41 +43,53 @@ public class LEAP_coordinates : MonoBehaviour
             float[] ring = { 0, 0, 0 };
             float[] pinky = { 0, 0, 0 };
             float[] thumb = { 0, 0, 0 };
+            float[] newCenter = { 0, 0, 0 };
 
+            for (uint i = 0; i < 3; i++)
+            {
+                newCenter[i] = handCenter[i] - handCenter[i];
+            }
             for (uint i = 0; i < 3 ; i++)
             {
-                index[i] = indexPosition[i];// - handCenter[i];
+                index[i] = indexPosition[i] - handCenter[i];
             }
             for (uint i = 0; i < 3; i++)
             {
-                middle[i] = middlePosition[i];// - handCenter[i];
+                middle[i] = middlePosition[i] - handCenter[i];
             }
             for (uint i = 0; i < 3; i++)
             {
-                ring[i] = ringPosition[i];// - handCenter[i];
+                ring[i] = ringPosition[i] - handCenter[i];
             }
             for (uint i = 0; i < 3; i++)
             {
-                pinky[i] = pinkyPosition[i];// - handCenter[i];
+                pinky[i] = pinkyPosition[i] - handCenter[i];
             }
             for (uint i = 0; i < 3; i++)
             {
-                thumb[i] = thumbPosition[i];// - handCenter[i];
+                thumb[i] = thumbPosition[i] - handCenter[i];
             }
 
-            float[] fingertips = { index[0], index[1], index[2], middle[0], middle[1], middle[2], ring[0], ring[1], ring[2], pinky[0], pinky[1], pinky[2], thumb[0], thumb[1], thumb[2]};
+            float[] fingertips = { index[0], index[1], index[2], middle[0], middle[1], middle[2], ring[0], ring[1], ring[2], pinky[0], pinky[1], pinky[2], thumb[0], thumb[1], thumb[2], handCenter[0], handCenter[1], handCenter[2]};
 
-            //for (int i = 0; i < 15; i++)
-            //{
-            //    fingertips[i] = Mathf.Round(fingertips[i]*1000)/1000;
-            //}
-            
-            
-            //Debug.Log(handCenter[0] + "," + handCenter[1] + "," + handCenter[2]);
-            //Debug.Log(indexPosition[0] + "," + indexPosition[1] + "," + indexPosition[2]);
+            for (int i = 0; i < 18; i++)
+            {
+                fingertips[i] = Mathf.Round(fingertips[i]*10)/10;
+            }
+
+            /*
+            Debug.Log("Center" + handCenter[0] + "," + handCenter[1] + "," + handCenter[2]);
+            Debug.Log("Old Index" + indexPosition[0] + "," + indexPosition[1] + "," + indexPosition[2]);
+            Debug.Log("New Index" + index[0] + "," + index[1] + "," + index[2];
+            Debug.Log("Middle" + middlePosition[0] + "," + middlePosition[1] + "," + middlePosition[2]);
+            Debug.Log("Ring" + ringPosition[0] + "," + ringPosition[1] + "," + ringPosition[2]);
+            Debug.Log("Pinky" + pinkyPosition[0] + "," + pinkyPosition[1] + "," + pinkyPosition[2]);
+            Debug.Log("Thumb" + thumbPosition[0] + "," + thumbPosition[1] + "," + thumbPosition[2]);
+            */
+
             OscMessage message = new OscMessage();
             message.address = "/wek/inputs";
-            for (int i = 0; i < 15; i++)
+            for (int i = 0; i < 18; i++)
             {
                 message.values.Add(fingertips[i]);
             }
